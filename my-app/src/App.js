6cd427aa94162885ac6666d7508e67db9d80b4c7
litwein <nicklitvinow@gmail.com>
2025-02-02 01:31:49 +0700

    - Add Email Me icon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,17 @@ import Header from './components/Header';
 import SocialLinks from './components/SocialLinks';
 import GitHubCalendarWidget from './components/GitHubCalendarWidget';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
       <div className="app">
-        <Header />
-        <SocialLinks />
-        <GitHubCalendarWidget />
-        <Projects />
+          <Header />
+          <Contact />
+          <SocialLinks />
+          <GitHubCalendarWidget />
+          <Projects />
       </div>
   );
 }
---
+++ my-app/src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaPaperPlane } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <motion.div
+            className="contact-minimal"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+        >
+            <a href="mailto:nicklitvinow@gmail.com" className="contact-link">
+                <FaPaperPlane className="paper-plane-icon" />
+                <span>Email Me</span>
+            </a>
+        </motion.div>
+    );
+};
+
+export default Contact;
